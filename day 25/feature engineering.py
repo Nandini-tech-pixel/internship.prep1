cb111ad343e00c7b846eedd7334419e856f11027
Nandini-tech-pixel <nan.off.1824@gmail.com>
2025-07-24 01:53:29 +0530

    Delete day 25 directory

--- day 25/feature engineering.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-df = pd.read_csv("titanic_sample.csv")
-# Feature: Title from Name
-df['Title'] = df['Name'].str.extract(' ([A-Za-z]+)\.', expand=False)
-# Feature: Family Size
-df['FamilySize'] = df['SibSp'] + df['Parch'] + 1
-# Feature: Age Group
-df['AgeGroup'] = pd.cut(df['Age'], bins=[0, 12, 18, 50, 100], labels=["Child", "Teen", "Adult", "Senior"])
-print(df[['Name', 'Title', 'FamilySize', 'AgeGroup']].head())
